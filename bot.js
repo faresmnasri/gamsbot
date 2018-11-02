289dc50cd9730dec144a04a62fa2cac6323d0a8c
salamri <42224359+salamri@users.noreply.github.com>
2018-11-03 02:19:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,4 +19,34 @@ client.on('message', msg => {
   }
 });
 
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+ client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("Check your DM")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('Requested by: ' + message.author.tag)//حقوق دايموند كودز
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("Link duration: 1 hour | Number of ueses of the link: 1**")
+      message.author.sendEmbed(Embed11)
+    }
+}); 
+
+
  client.login(process.env.BOT_TOKEN);
